5282282a181adcbd12adfaed03cacdb7843a88e1
fishku <juerui@vip.qq.com>
2019-12-07 14:39:55 +0800

    1-项目初始化

---
+++ MyHomePage.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class MyHomePage extends Component {
+  // constructor: 一般都要写出来(一般都要挂载数据)
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  // 必须要有render 函数
+  render () {
+    // 1.在RN中,不能使用网页中的所有标签
+    // 2.如果想要实现布局,RN提供了 View的组件,来实现布局;
+    // 3.RN提供了一系列基础的组件，来方便程序员的开发，如果想要使用这些组件,只需把组件从'react-native'中导入即可
+    return (
+      <View>
+        {/* 所有的文本,必须要用RN提供的Text组件包裹,否则报错  */}
+        <Text>123</Text>
+      </View>
+    );
+  }
+}
+
+export default MyHomePage;
---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * @format
+ */
+
+import { AppRegistry } from 'react-native';
+import App from './App';
+import { name as appName } from './app.json';
+// 当使用APPRegister注册项目的时候,方法中的第一个参数，不要改，否则项目废了
+// 第二个参数，表示要把哪个页面注册为项目的首页
+// 在RN中,只能使用.js作为组件的后缀名,不能使用.jsx
+AppRegistry.registerComponent(appName, () => App);

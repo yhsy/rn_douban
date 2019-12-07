3f491aadf0574f98dc5b0569cbd5d4ecf6325eda
fishku <juerui@vip.qq.com>
2019-12-07 15:06:33 +0800

    2-修改首页入口文件

--- MyHomePage.js
+++ MyHomePage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 class MyHomePage extends Component {
   // constructor: 一般都要写出来(一般都要挂载数据)
@@ -14,12 +14,26 @@ class MyHomePage extends Component {
     // 2.如果想要实现布局,RN提供了 View的组件,来实现布局;
     // 3.RN提供了一系列基础的组件，来方便程序员的开发，如果想要使用这些组件,只需把组件从'react-native'中导入即可
     return (
-      <View>
+      <View style={styles.mHd}>
         {/* 所有的文本,必须要用RN提供的Text组件包裹,否则报错  */}
-        <Text>123</Text>
+        <Text style={styles.title}>MyHomePage123</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  mHd: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    height: 100,
+    backgroundColor: '#f00',
+  },
+  title: {
+    fontSize: 20,
+    color: '#fff',
+    textAlign: 'center'
+  }
+})
 
 export default MyHomePage;
